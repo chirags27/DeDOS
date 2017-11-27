fe573b2c966c3451de429b753aff19be69b77c30
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2017-11-27 20:22:02 +0000

    Deploy dedos-project/DeDOS to github.com/dedos-project/DeDOS.git:gh-pages

---
+++ docs/search/files_5.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['haproxy_2ec',['haproxy.c',['../haproxy_8c.html',1,'']]],
+  ['haproxy_2eh',['haproxy.h',['../haproxy_8h.html',1,'']]],
+  ['http_5fmsu_2ec',['http_msu.c',['../http__msu_8c.html',1,'']]],
+  ['http_5fmsu_2eh',['http_msu.h',['../http__msu_8h.html',1,'']]],
+  ['http_5fparser_2ec',['http_parser.c',['../http__parser_8c.html',1,'']]],
+  ['http_5fparser_2eh',['http_parser.h',['../http__parser_8h.html',1,'']]]
+];
---
+++ src/msus/webserver/write_msu.c
@@ -0,0 +1,78 @@
+/*
+START OF LICENSE STUB
+    DeDOS: Declarative Dispersion-Oriented Software
+    Copyright (C) 2017 University of Pennsylvania
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+END OF LICENSE STUB
+*/
+#include "webserver/write_msu.h"
+#include "socket_msu.h"
+#include "webserver/connection-handler.h"
+#include "msu_state.h"
+#include "logging.h"
+#include "routing_strategies.h"
+#include "profiler.h"
+
+static int write_http_response(struct local_msu *self,
+                               struct msu_msg *msg) {
+    struct response_state *resp_in = msg->data;
+
+    size_t size = 0;
+    struct response_state *resp = msu_get_state(self, &msg->hdr.key, &size);
+    if (resp == NULL) {
+        resp = msu_init_state(self, &msg->hdr.key, sizeof(*resp));
+        memcpy(resp, resp_in, sizeof(*resp_in));
+    }
+
+    if (resp->conn.status == CON_ERROR) {
+        msu_remove_fd_monitor(resp->conn.fd);
+        close_connection(&resp->conn);
+        msu_free_state(self, &msg->hdr.key);
+        free(resp_in);
+        return 0;
+    }
+
+    int rtn = write_response(resp);
+    if (rtn & WS_ERROR) {
+        msu_remove_fd_monitor(resp->conn.fd);
+        close_connection(&resp->conn);
+        msu_free_state(self, &msg->hdr.key);
+        free(resp_in);
+        return -1;
+    } else if (rtn & (WS_INCOMPLETE_READ | WS_INCOMPLETE_WRITE)) {
+        rtn = msu_monitor_fd(resp->conn.fd, RTN_TO_EVT(rtn), self, &msg->hdr);
+        free(resp_in);
+        return rtn;
+    } else {
+        PROFILE_EVENT(msg->hdr, PROF_DEDOS_EXIT);
+        msu_remove_fd_monitor(resp->conn.fd);
+        close_connection(&resp->conn);
+        log(LOG_WEBSERVER_WRITE, "Successful connection to fd %d closed",
+                   resp->conn.fd);
+        log(LOG_WEBSERVER_WRITE, "Wrote request: %s", resp->resp);
+        msu_free_state(self, &msg->hdr.key);
+        free(resp_in);
+        return 0;
+    }
+}
+
+struct msu_type WEBSERVER_WRITE_MSU_TYPE = {
+    .name = "Webserver_write_MSU",
+    .id = WEBSERVER_WRITE_MSU_TYPE_ID,
+    .receive = write_http_response,
+    .route = route_to_origin_runtime
+};
+
+
